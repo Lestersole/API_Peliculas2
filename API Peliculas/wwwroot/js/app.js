ede09330aba8c08d77eca675e5db53f177a88ae6
Lestersole <veraadrian59@gmail.com>
2024-10-19 23:52:55 -0500

    Funcionalidad de busqueda por categoria

--- API Peliculas/wwwroot/js/app.js
+++ API Peliculas/wwwroot/js/app.js
@@ -1,7 +1,9 @@
 ﻿var app = angular.module('miApp', []);
 
 app.controller('MiControlador', ['$scope', '$http', function ($scope, $http) {
+    $scope.categorias = ["Accion", "Comedia", "Drama", "Terror"];
     $scope.peliculas = [];
+    $scope.categoriaSeleccionada = "";
 
     $scope.cargarPeliculas = function () {
         $http.get('https://localhost:7008/api/PeliculasControlador/GetPeliculas')  // URL del backend
@@ -19,7 +21,7 @@ app.controller('MiControlador', ['$scope', '$http', function ($scope, $http) {
 
     $scope.buscarPorCategoria = function () {
         if ($scope.categoriaSeleccionada) {
-            $http.get('https://localhost:7008/api/PeliculasControlador/GetCategoria/' + $scope.categoriaSeleccionada)
+            $http.get('https://localhost:7008/api/PeliculasControlador/GetCategoria?categoria=' + $scope.categoriaSeleccionada)
             .then(function (response) {
                 $scope.peliculas = response.data.value;
                 console.log("Películas cargadas:", $scope.peliculas);
